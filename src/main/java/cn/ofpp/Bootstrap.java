d9560c51e45fec289c31aef12e906b4bc3450358
lumen <78414343+MetaOrigin@users.noreply.github.com>
2022-08-23 20:43:07 +0800

    Update Bootstrap.java
    FIX: myself

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -14,17 +14,17 @@ public class Bootstrap {
     /**
      * 公众号AppID
      */
-    public static final String APP_ID = "";
+    public static final String APP_ID = "wxf0e133c91813d6da";
 
     /**
      * 公众号秘钥
      */
-    public static final String SECRET = "";
+    public static final String SECRET = "a5ae8f4259498347892de170d6753b8e";
 
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "";
+    public static final String TEMPLATE_ID = "9_bHdDuyWcAMRbJ0nd8-wwKRuwkn7oWuRURlHGOTq1A	";
 
     /**
      * 初始化
